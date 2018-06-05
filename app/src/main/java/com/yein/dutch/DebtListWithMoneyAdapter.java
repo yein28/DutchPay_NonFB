ec227ede8e517c680361e14b6d93b9ece83c76b6
yein28 <gkshs1234@gmail.com>
2018-06-05 20:11:54 +0900

    DutchComplete에서 보여줄 리스트의 경우 기존의 DebtListMoneyAdapter에서 인원수에 맞추어 나눈 금액만 함께 보여주면 되지만 재사용할 방법을 찾지 못함

---
+++ app/src/main/java/com/yein/dutch/DebtListWithMoneyAdapter.java
@@ -0,0 +1,54 @@
+package com.yein.dutch;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class DebtListWithMoneyAdapter extends BaseAdapter{
+    private ArrayList<Member> members;
+
+    DebtListWithMoneyAdapter(ArrayList<Member> members){
+        this.members = members;
+    }
+
+    @Override
+    public int getCount() {
+        return members.size();
+    }
+
+    @Override
+    public Member getItem(int i) {
+        return members.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        final Context context = viewGroup.getContext();
+        if( view == null ){
+            LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.id_ratingbar_money, viewGroup, false);
+        }
+
+        TextView id = view.findViewById(R.id.tv_complete_debt_id);
+        RatingBar ratingBar = view.findViewById(R.id.rb_complete_debt_rate);
+        TextView money = view.findViewById(R.id.tv_complete_debt_money);
+
+        id.setText(getItem(i).getId());
+        ratingBar.setRating(Float.parseFloat(getItem(i).getRate()));
+        String m = Integer.toString(getItem(i).getMoney());
+        money.setText(m);
+
+        return view;
+    }
+}
